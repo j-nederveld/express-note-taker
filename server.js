2d051179d16c6e5913db71f8d77d1cd1795245bd
John Nederveld <john.nederveld@gmail.com>
2020-10-08 21:04:52 -0400

    I am big dum and did not add the path requirement in server js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
+var path = require('path')
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 
